5f9d9c21c0267f2b347acfc9209c30df00fcc323
Holger Thies <info@holgerthies.com>
2021-04-28 19:44:39 +0900

    finished sqrt approx

--- newreal/sqrt.v
+++ newreal/sqrt.v
@@ -45,29 +45,6 @@ Proof.
   by apply IZReal_relator.
 Qed.
 
-Definition sqrt_approx : forall (x x0 : Real), (Real0 <= x) -> Real0 < x0 -> forall (n : nat), {y | Real0 < y}.
-Proof.
-  move => x x0 xge0 x0gt0.
-  elim =>[| n' [y IH]]; first by exists x0.
-  have yneq0 : y <> Real0.
-  - classical.
-    relate.
-    have := (relator_lt _ _ _ _ relator_constant0 H IH).
-    by lra.
-  exists (/ dReal2 * (y + x / yneq0)).
-  rewrite /Realdiv.
-  classical. 
-  relate.
-  apply Rmult_lt_0_compat; first by rewrite (relator_IZReal _ _ Ha3); lra.
-  apply Rplus_lt_le_0_compat.
-  - by apply (relator_lt _ _ _ _ relator_constant0 Ha2).
-  apply Rcomplements.Rdiv_le_0_compat.
-  - apply /transport_leq_inv/xge0 => //.
-    apply relator_constant0.
-  apply /relator_lt/IH => //.
-  by apply relator_constant0.
-Defined.
-
 Lemma IZReal4neq0 : IZReal 4 <> Real0.
 Proof.
   classical.
@@ -76,4 +53,110 @@ Proof.
   by lra.
 Qed.
 
- 
+Lemma sqrt_bound_helper x :  (/ IZReal4neq0) <= x -> (x <= Real2) -> forall rx, relator x rx -> ((/ 4) <= rx <= 2)%R.
+Proof.
+  move => Bl Bu rx RX.
+  split.
+  - apply /transport_leq_inv/Bl => //.
+    by apply  (relator_divison _ IZReal4neq0 4%R 4 (IZReal_relator 4)).
+  apply /transport_leq_inv/Bu => //.
+  by apply IZReal_relator.
+Qed.
+
+Lemma sqrt_bounds x :  (/ IZReal4neq0) <= x -> (x <= Real2) -> forall rx, relator x rx -> ((/ 2) <= (sqrt rx) <= (sqrt 2))%R.
+Proof.
+  move => Bl Bu rx RX.
+  have [B1 B2] := sqrt_bound_helper x Bl Bu rx RX.
+  suff -> : (/ 2)%R = (sqrt (/ 4)) by split;apply sqrt_le_1;lra.
+  rewrite <-inv_sqrt; last by lra.
+  have -> : (4 = 2 * 2)%R by lra.
+  by rewrite sqrt_square; lra.
+Qed.
+
+Lemma sqrt_expand x : (0 < x)%R -> (sqrt x) = ((/ 2)*((sqrt x) + (x / sqrt x)))%R.
+Proof.
+  move => H.
+  field_simplify_eq.
+  rewrite //= Rmult_1_r sqrt_sqrt; lra.
+  suff : (0 < sqrt x)%R by lra.
+  by apply sqrt_lt_R0.
+Qed.
+
+Definition sqrt_approx x n : (/ IZReal4neq0) <= x -> (x <= Real2) -> {y | forall rx ry, relator x rx -> relator y ry -> (Rabs (ry - sqrt rx) <= (/ 2 ^ (2 ^ n)))%R}.
+Proof.
+  move => B1 B2.
+  suff [y P] : {y | (Real0 < y) /\ forall rx ry, relator x rx -> relator y ry -> (ry = 1 \/ sqrt rx <= ry)%R /\ (Rabs (ry - sqrt rx) <= (/ 2 ^ (2 ^ n)))%R} by exists y; apply P.
+  elim n =>[| n' [y [ygt0 IH]]].
+  - exists Real1; split => [| rx ry RX RY]; first by apply Real1_gt_Real0.
+    split; first by relate; auto.
+    have [B1' B2'] := (sqrt_bound_helper _ B1 B2 _ RX).
+    relate.
+    apply Rcomplements.Rabs_le_between' => /=.
+    split;ring_simplify; first by interval.
+    suff : ((/ 2) <= sqrt rx)%R by lra.
+    suff -> : (/ 2)%R = (sqrt (/ 4)) by apply sqrt_le_1;lra.
+    rewrite <-inv_sqrt; last by lra.
+    have -> : (4 = 2 * 2)%R by lra.
+    by rewrite sqrt_square; lra.
+  have yneq0 : y <> Real0 by apply Realgt_neq.
+  exists (/ dReal2 * (y + x / yneq0)).
+  split.
+  - classical. 
+    relate.
+    have [B1' B2'] := (sqrt_bound_helper _ B1 B2 _ Ha1).
+    have ygt0' : (0 < x1)%R by apply /transport_lt_inv/ygt0/Hb/relator_constant0.
+    rewrite (relator_IZReal _ _ Ha2).
+    apply Rmult_lt_0_compat; first by lra.
+    apply Rplus_lt_0_compat; first by lra.
+    by apply Rdiv_lt_0_compat; lra.
+  move => rx ry RX RY.
+  relate.
+  rewrite (relator_IZReal _ _ Ha2).
+  have [ygt IH'] := IH _ _ Ha1 Hb. 
+  have [B1' B2'] := (sqrt_bound_helper _ B1 B2 _ Ha1).
+  have ygt0' : (0 < x1)%R by apply /transport_lt_inv/ygt0/Hb/relator_constant0.
+  have p : (0 < rx / x1)%R by apply Rdiv_lt_0_compat; lra.
+  split.
+  - apply or_intror. 
+    rewrite <- sqrt_pow2; last by lra.
+    apply sqrt_le_1; [by lra| by apply pow2_ge_0|].
+    rewrite Rpow_mult_distr.
+    have -> : (x1 + (rx/x1))^2 = (x1^2 + 2*rx + (rx/x1)^2)%R by field_simplify_eq;lra.
+    suff : (0 <= (x1 ^ 2 - 2*rx + (rx/x1)^2))%R by lra.
+    have -> : (x1^2 - 2*rx + (rx / x1)^2 = ((x1-(rx/x1)) ^ 2))%R by field_simplify_eq;lra.
+    by apply pow2_ge_0.
+  rewrite sqrt_expand; last by lra.
+  rewrite <- Rmult_minus_distr_l, Rabs_mult, Rabs_right; last by lra.
+  have -> : (x1 + (rx/x1) - ((sqrt rx) + (rx / sqrt rx)) = (x1 - (sqrt rx))+(rx/x1 - (rx / sqrt rx)))%R by lra.
+  have -> : (x1 - (sqrt rx) + ((rx/x1) - (rx / sqrt rx)) = (x1 - sqrt rx)*(x1 - (rx / sqrt rx ))*(/ x1))%R.
+  - field_simplify_eq;try by lra.
+    split; try by lra.
+    by interval.
+ have -> : ((x1 - (sqrt rx))*(x1 - (rx / sqrt rx)) = ((x1 - (sqrt rx)) ^ 2))%R.
+ - field_simplify_eq; try by interval.
+   rewrite /= !Rmult_1_r.
+   by rewrite !sqrt_sqrt; lra.
+  suff H : (Rabs (/ x1) <= 2)%R.
+  - rewrite Rabs_mult.
+    ring_simplify.
+    rewrite <- RPow_abs.
+    apply /Rle_trans.
+    apply Rmult_le_compat_l; first by apply Rmult_le_pos; [lra|apply pow2_ge_0].
+    apply H.
+    suff : ((Rabs (x1 - sqrt rx))^2 <= (/ 2 ^ (2 ^ (n'.+1))))%R by lra.
+    have -> : ((2 ^ (n'.+1)) = ((2 ^ n')*2))%nat by rewrite Nat.pow_succ_r' Nat.mul_comm.
+    rewrite pow2_abs.
+    rewrite pow_mult.
+    rewrite Rinv_pow; last by apply pow_nonzero;lra.
+    by apply pow_maj_Rabs.
+  rewrite Rabs_Rinv; last by lra.
+  rewrite Rabs_right; last by lra.
+  have -> : (2 = / / 2)%R by lra.
+  apply Rle_Rinv; try by lra.
+  case ygt => H; first by lra.
+  suff: (/ 2 <= sqrt rx)%R by lra.
+  suff -> : ((/ 2) = (sqrt (/ 4)))%R by apply sqrt_le_1;lra.
+  have -> : ((/ 4) = (/ 2) ^ 2)%R by lra.
+  rewrite sqrt_pow2;lra.
+Defined.
+
