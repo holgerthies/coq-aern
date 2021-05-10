149e40c3f909927236cd2eba5964ec7576af83fa
Sewon Park <swelite@kaist.ac.kr>
2021-05-10 20:09:48 +0900

    reorganize file

--- newreal/Base.v
+++ newreal/Base.v
@@ -10,3 +10,69 @@ Proof.
   rewrite H.
   exact (eq_refl _).
 Defined.
+
+
+(* transporting path *)
+Definition tpp : forall A : Type, forall P : A -> Type, forall x y : A, forall e : x = y, P x -> P y.
+Proof.
+  intros.
+  induction e.
+  exact X.
+Defined.
+
+(* path of sigma types *)
+Lemma sewonsewon : forall (A : Type) (P : A -> Type) (x y : A) (a : P x) (b : P y), forall e : x = y,
+      tpp A P x y e a = b -> existT P x a = existT P y b.
+Proof.
+  intros.
+  induction H.
+  unfold tpp.
+  unfold eq_rect.
+
+  destruct e.
+  exact eq_refl.
+Defined.
+
+(* path of sigma types *)
+Lemma sewonsewonp : forall (A : Type) (P : A -> Prop) (x y : A) (a : P x) (b : P y), forall e : x = y,
+      tpp A P x y e a = b -> exist P x a = exist P y b.
+Proof.
+  intros.
+  induction H.
+  unfold tpp.
+  unfold eq_rect.
+
+  destruct e.
+  exact eq_refl.
+Defined.
+
+
+Definition sewon_sewon : forall A P (a c : A) b d, existT P a b = existT P c d -> a = c.
+Proof.
+  intros.
+  auto.
+  apply (@lp {x : A & P x} A (@projT1 A P  ) (existT P a b) (existT P c d)) in H.
+  simpl in H.
+  exact H.
+Defined.
+
+Definition projP1 : forall A (P : A -> Prop) (x : {x : A | P x}), A.
+Proof.
+  intros.
+  destruct x.
+  exact x.
+Defined.
+
+
+Definition sewon_sewonp : forall A P (a c : A) b d, exist P a b = exist P c d -> a = c.
+Proof.
+  intros.
+  apply (@lp {x : A | P x} A (@projP1 A P  ) (exist P a b) (exist P c d)) in H.
+  simpl in H.
+  exact H.
+Defined.
+  
+(* equivalence in extensional type theory *)
+Definition id (A : Type) : A -> A := fun x => x.
+Definition fc {A B C : Type} (f : B -> C) (g : A -> B) : A -> C := fun x => f (g x).
+Definition is_equiv {A B : Type} (f : A -> B) := {g : B -> A | fc g f = id _ /\ fc f g = id _}.
