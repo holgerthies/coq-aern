d3b4686c2aee142878256fb2290fc7f34ee1e57d
Holger Thies <info@holgerthies.com>
2021-07-08 13:00:23 +0900

    proved remaining lemmas for magnitude

--- formalization/testsearch.v
+++ formalization/testsearch.v
@@ -275,21 +275,110 @@ Qed.
 
 Definition is_magnitude x z := Zpow _ Real2_neq_Real0 (z - 2) <= x <= Zpow _ Real2_neq_Real0 z. 
 
-Lemma Zpow_case x xneq0 z : Zpow x xneq0 z = RealRing.pow x (Z.to_nat z) \/ Zpow x xneq0 z = RealRing.pow (/ xneq0) (Z.to_nat (- z)).
+Lemma inv_neq0 (x : Real) (xneq0 : x <> Real0) : (/ xneq0) <> Real0. 
+Proof.
+  classical.
+  relate.
+  apply Rinv_neq_0_compat.
+  by apply /transport_neq_inv/xneq0/relate_constant0/Ha.
+Qed.
+
+
+Lemma Zpow_pos x xneq0 z : (0 <= z)%Z -> Zpow x xneq0 z = RealRing.pow x (Z.to_nat z).
+Proof.
+  move => H.
+  by case e : z => //; lia.
+Qed.
+
+Lemma Zpow_neg x xneq0 z : (z <= 0)%Z -> Zpow x xneq0 z = RealRing.pow (/ xneq0) (Z.to_nat (-z)).
+Proof.
+  move => H.
+  by case e : z => // /=; try lia.
+Qed.
+
+Lemma pow_plus x n1 n2 : RealRing.pow x (n1+n2) = RealRing.pow x n1 * RealRing.pow x n2.
+Proof.
+  elim n2 => [| n' IH ]; first by rewrite /= Nat.add_0_r Realmult_comm Realmult_unit.
+  have ->  : ((n1 + n'.+1) = ((n1+n').+1))%coq_nat by lia.
+  rewrite Realmult_comm /= Realmult_assoc.
+  apply Realeq_mult_eq.
+  by rewrite Realmult_comm.
+Qed.
+
+
+Lemma Zpow_inv x xneq0 z : Zpow x xneq0 (-z) = Zpow (/ xneq0) (inv_neq0 _ xneq0) z.
 Proof.
   case (Z.neg_nonneg_cases z) => H.
-  right.
-  - by case e : z => [| p | p] // /=; try lia.
-  left.
-  by case e : z => [| p | p] // /=; try lia.
+  - rewrite Zpow_pos; try by lia.
+    rewrite Zpow_neg; try by lia.
+    suff -> : (/ inv_neq0 x xneq0) = x by trivial. 
+    classical.
+    relate.
+    apply Rinv_involutive.
+    by apply /transport_neq_inv/xneq0/relate_constant0/Ha0.
+  rewrite Zpow_neg; try by lia.
+  rewrite Zpow_pos; try by lia.
+  by rewrite Z.opp_involutive.
+Qed.
+
+Lemma Zpow_succ x xneq0 z : Zpow x xneq0 (Z.succ z) = x * Zpow x xneq0 z.
+Proof.
+  case (Z.neg_nonneg_cases z) => H;case (Z.neg_nonneg_cases (Z.succ z)) => H'; try by lia.
+  - rewrite !Zpow_neg; try lia.
+    have -> : (- z = -Z.succ z  + 1)%Z by lia. 
+    rewrite Z2Nat.inj_add; try by lia.
+    have -> : (Z.to_nat (- Z.succ z) + Z.to_nat 1 = (Z.to_nat (- Z.succ z)).+1)%coq_nat by lia.
+    rewrite /= -Realmult_assoc.
+    suff -> : x * / xneq0 = Real1 by rewrite Realmult_unit.
+    rewrite Realmult_comm.
+    by apply Realmult_inv.
+  - have -> : (z = -1)%Z by lia.
+    rewrite /=.
+    by rewrite  /= -Realmult_assoc Realmult_comm Realmult_unit Realmult_comm Realmult_inv.
+  rewrite !Zpow_pos; try by lia.
+  rewrite Z2Nat.inj_add; try by lia.
+  have -> : (Z.to_nat z + Z.to_nat 1 = (Z.to_nat z).+1)%coq_nat by lia.
+  by auto.
+Qed.
+
+Lemma Zpow_plus x xneq0 z1 z2 : Zpow x xneq0 (z1+z2) = Zpow x xneq0 z1  * Zpow x xneq0 z2.
+  move : z2.
+  apply Z.bi_induction => [x0 y -> | | z] => // /=; first by rewrite Z.add_0_r Realmult_comm Realmult_unit.
+  split => H.
+  - have -> : (z1 + Z.succ z = Z.succ (z1+z) )%Z by lia.
+    rewrite !Zpow_succ.
+    by rewrite -Realmult_assoc (Realmult_comm _ x) H Realmult_assoc.
+  have T y u1 u2 : x * y = u1 * (x * u2) -> y = u1 * u2 by clear H;intros;Holger H;Holger xneq0;classical;relate;nra. 
+  apply T.
+  rewrite -!Zpow_succ.
+  have -> : (Z.succ (z1+z) = z1 + Z.succ z)%Z by lia.
+  exact H.
 Qed.
 
 Lemma magnitude_half x z : is_magnitude (x / Real2_neq_Real0) z -> is_magnitude x (z+1).
 Proof.
   move => [H1 H2].
   split.
-  simpl.
-Admitted.
+  - have -> : (z + 1 - 2 = z - 1)%Z by lia.
+    move : H1.
+    rewrite !Zpow_plus /= => H1.
+    classical; relate.
+
+    have -> : (x2 = 2)%R by apply relate_IZReal.
+
+    suff : (x1 * (/ 2 * (/ 2 * 1) ) <= y / 2)%R by lra.
+    apply /transport_leq_inv/H1/relate_multiplication/relate_divison/IZReal_relator/H0.
+    apply /relate_multiplication/relate_multiplication/relate_multiplication/relate_constant1/relate_divison/IZReal_relator/relate_divison/IZReal_relator.
+    apply Ha.
+  rewrite Zpow_plus /=.
+  classical;relate.
+  have -> : (x2 = 2)%R by apply relate_IZReal.
+  suff : (x0 / 2 <= x1)%R by lra.
+  by apply /transport_leq_inv/H2/Ha/relate_multiplication/relate_divison/IZReal_relator/H.
+Qed.
+
+
+
 Lemma magnitude_fourth x z : is_magnitude (x /IZReal4neq0) z -> is_magnitude x (z+2).
 Proof.
   suff -> : (x / IZReal4neq0) = (x / Real2_neq_Real0 / Real2_neq_Real0).
@@ -304,9 +393,6 @@ Proof.
   by lra.
 Qed.
 
-Lemma magnitude_inv x (xneq0 : x<> Real0) z : is_magnitude (/ xneq0) z -> is_magnitude x (-z+2).
-Proof.
-Admitted.
 
 (* first extend magnitude to numbers <= 2 *)
 Definition magnitude2 x : (Real0 < x < Real2) -> M { z | is_magnitude x z }.
@@ -336,6 +422,49 @@ Proof.
   have -> : ((- Z.of_nat n - 2) = (- Z.of_nat (n.+2)%coq_nat))%Z by lia.
   by rewrite Zpow_prec; apply Reallt_le;apply nprp.
 Qed.
+
+
+Lemma Zpow_relate x xneq0 z xr: relate x xr -> relate (Zpow x xneq0 z) (powerRZ xr z). 
+Proof.
+  move => R.
+  have xrneg0 : (xr <> R0)%R by Holger xneq0;relate.
+  move : z.
+  apply Z.bi_induction => [x0 y -> | | z] => // /=; first by apply relate_constant1.
+  split => H.
+  - rewrite Zpow_succ powerRZ_add /= => //.
+    have ->: forall p, (p * (xr * 1) = xr * p)%R by intros;lra. 
+    by apply relate_multiplication.
+  move : H.
+  rewrite Zpow_succ powerRZ_add /= => //.
+  have ->: forall p, (p * (xr * 1) = xr * p)%R by intros;lra. 
+  move => H.
+  have := relate_multiplication _ _ _ _ (relate_divison _ xneq0 _ R) H.
+  rewrite -Realmult_assoc -Rmult_assoc Realmult_inv Realmult_unit Rinv_l => //.
+  by rewrite Rmult_1_l.
+Qed.
+
+Lemma magnitude_inv x (xneq0 : x<> Real0) z : is_magnitude (/ xneq0) z -> is_magnitude x (-z+2).
+Proof.
+  move => [H1 H2].
+  have R1 := Zpow_relate Real2 Real2_neq_Real0 (z-2) _ (IZReal_relator 2).
+  have R2 := Zpow_relate Real2 Real2_neq_Real0 z _ (IZReal_relator 2). 
+  have R3 := Zpow_relate Real2 Real2_neq_Real0 (-z + 2 - 2) _ (IZReal_relator 2). 
+  have R4 := Zpow_relate Real2 Real2_neq_Real0 (-z + 2 ) _ (IZReal_relator 2). 
+  have P := (powerRZ_lt 2 z Rlt_0_2).
+  have P2 := (powerRZ_lt 2 (z-2) Rlt_0_2).
+  split; classical;relate;Holger H1;Holger H2;relate;Holger xneq0;relate.
+  - have -> : (-z + 2 - 2 = -z)%Z by lia.
+    rewrite powerRZ_neg; try by lra.
+    rewrite powerRZ_inv; try by lra.
+    rewrite -(Rinv_involutive y); try by lra.
+    apply Rle_Rinv; try by lra.
+  have -> : (-z + 2 = -(z-2))%Z by lia.
+  rewrite powerRZ_neg; try by lra.
+  rewrite powerRZ_inv; try by lra.
+  rewrite -(Rinv_involutive x0); try by lra.
+  by apply Rle_Rinv; try by lra.
+Qed.
+
 
 Lemma magnitude x : Real0 < x -> M {z | is_magnitude x z}.
 Proof.
