8cc328c7308de4106636e481bef216560b3cceaa
sewon park <swelite@kaist.ac.kr>
2022-08-23 16:57:00 +0900

    rearrange

--- formalization/Subsets.v
+++ formalization/Subsets.v
@@ -20,17 +20,32 @@ Section Subsets.
   
   Definition csubset (n : nat) := @euclidean Real n -> Prop. 
 
-  Definition Hausdorff_dist_bound {n : nat} (S T : csubset n) d :=
-    (forall x, S x -> exists y, T y /\ euclidean_max_dist x y <= d) /\
-      (forall y, T y -> exists x, S x /\ euclidean_max_dist x y <= d).
-  
-  
   Definition union {n : nat} : (csubset n) -> (csubset n) -> (csubset n).
   Proof.
     intros S T x.
     exact (S x \/ T x).
   Defined.
 
+  Definition intersection {n : nat} : (csubset n) -> (csubset n) -> (csubset n).
+  Proof.
+    intros S T x.
+    exact (S x /\ T x).
+  Defined.
+
+  Definition is_subset {n : nat} : (csubset n) -> (csubset n) -> Prop :=
+    fun S T => forall x, S x -> T x.
+
+  Infix "∪" := union (at level 80).
+  Infix "∩" := intersection (at level 80).
+  Infix "⊆" := is_subset (at level 85).
+  
+  Definition Hausdorff_dist_bound {n : nat} (S T : csubset n) d :=
+    (forall x, S x -> exists y, T y /\ euclidean_max_dist x y <= d) /\
+      (forall y, T y -> exists x, S x /\ euclidean_max_dist x y <= d).
+
+  Definition emptyset {n : nat} : csubset n := fun x => False.
+  Definition singleton {n : nat} x : csubset n := fun y => y = x.
+  
   Require Import List.
   
   Fixpoint list_union {n : nat}  (l : list (csubset n)) : csubset n :=
@@ -48,21 +63,36 @@ Section Subsets.
   Definition closed_ball {n : nat} (c : euclidean n) (r : Real) :=
     fun x => euclidean_max_dist x c <= r.
 
-  Definition is_compact {n : nat} (K : csubset n) :=
-    forall p,
+End Subsets.
+
+Section Compact.
+  Generalizable Variables K M Real.
+
+  Context `{klb : LazyBool K} `{M_Monad : Monad M}
+    {MultivalueMonad_description : Monoid_hom M_Monad NPset_Monad} 
+    {M_MultivalueMonad : MultivalueMonad}
+    {Real : Type}
+    {SemiDecOrderedField_Real : SemiDecOrderedField Real}
+    {ComplArchiSemiDecOrderedField_Real : ComplArchiSemiDecOrderedField}.
+
+  Add Ring realRing : realTheory.
+  
+  Definition is_compact {n : nat} (K : csubset n) := forall p,
       {B : list (euclidean n) &
              (Forall (fun k => exists y, K y /\ euclidean_max_dist k y <= prec p) B)
-           /\
-             forall y, K y ->
-                       Exists (fun k => euclidean_max_dist k y <= prec p) B}.
+           /\ forall y, K y -> Exists (fun k => euclidean_max_dist k y <= prec p) B}.
 
-  Definition is_compact_lim {d : nat} :
+  Lemma is_compact_closed_ball {d : nat} :
+    forall c r, @is_compact d (closed_ball c r).  
+  Proof.
+    intros.
+    intro p.
+    induction p.
+  Admitted.
+  
+  Lemma is_compact_lim {d : nat} :
     forall k : csubset d,
-      (forall n : nat,
-          {X : csubset d &
-                 prod (is_compact X)
-                   (Hausdorff_dist_bound X k (prec n))
-      })  
+      (forall n : nat, {X : csubset d & prod (is_compact X) (Hausdorff_dist_bound X k (prec n))})  
       -> is_compact k.
   Proof.
     intros.
@@ -111,4 +141,4 @@ Section Subsets.
     apply (Exists_impl _ H7 H6).
   Defined.  
 
-End Subsets.
+End Compact.
