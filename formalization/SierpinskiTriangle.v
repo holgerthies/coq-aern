03feea5048119d9ead99725c787dea6fa4c0d9a1
mikkonecny <mikkonecny@gmail.com>
2022-09-01 15:04:33 +0900

    ST: cover base case proved

--- formalization/SierpinskiTriangle.v
+++ formalization/SierpinskiTriangle.v
@@ -54,8 +54,66 @@ Context {types : RealTypes} { casofReal : ComplArchiSemiDecOrderedField_Real typ
     apply (make_euclidean2 x y).
   Defined.
 
+  Definition weights123_valid (c1 c2 c3 : ^Real) : Prop :=
+    c1 >= real_0 /\ c2 >= real_0 /\ c3 >= real_0 /\ c1 + c2 + c3 = real_1.
+
   Definition inside_ST_hull (s : euclidean_subset 2) : Prop :=
-    forall pt : ^euclidean 2, s pt -> exists c1 c2 c3 : ^Real, pt = (ST_weighted_pt c1 c2 c3) /\ c1 >= real_0 /\ c1 >= real_0 /\ c3 >= real_0.
+    forall pt : ^euclidean 2, s pt -> exists c1 c2 c3 : ^Real, pt = (ST_weighted_pt c1 c2 c3) /\ weights123_valid c1 c2 c3.
+
+  Lemma weights12_c1_le_1 c1 c2 : 
+    real_0 <= c1 /\ real_0 <= c2 /\ c1 + c2 <= real_1
+    -> c1 <= real_1.
+  Proof.
+    intros [c1pos [c2pos c12sum]].
+
+    assert ((c1+real_0) <= (c1+c2)) as Temp.
+    apply real_le_plus_le. apply c2pos.
+    pose proof (real_le_le_le _ _ _ Temp c12sum) as Temp2.
+    rewrite real_plus_comm in Temp2.
+    rewrite real_plus_unit in Temp2.
+    auto.
+  Qed.
+
+  Lemma weights12_c2_le_1 c1 c2 : 
+    real_0 <= c1 /\ real_0 <= c2 /\ c1 + c2 <= real_1
+    -> c2 <= real_1.
+  Proof.
+    intros [c1pos [c2pos c12sum]].
+    rewrite real_plus_comm in c12sum.
+
+    apply (weights12_c1_le_1 c2 c1).
+    split; auto; split; auto.
+  Qed.
+
+  Lemma weights123_le_1 c1 c2 c3 : 
+    weights123_valid c1 c2 c3 -> 
+    c1 <= real_1 /\ c2 <= real_1 /\ c3 <= real_1.
+  Proof.
+    intros [c1pos [c2pos [c3pos c123sum]]].
+    apply real_eq_le in c123sum.
+
+    assert (real_0 <= c1 + c2) as c12pos.
+    rewrite <- (real_plus_unit real_0).
+    apply real_le_le_plus_le; auto.
+
+    assert (c1 + c2 <= real_1) as c12sum.
+    apply (weights12_c1_le_1 (c1+c2) c3).
+    split; auto; split; auto.
+
+    split.
+    apply (weights12_c1_le_1 c1 c2).
+    split; auto; split; auto.
+
+    split.
+    apply (weights12_c2_le_1 c1 c2).
+    split; auto; split; auto.
+
+    apply (weights12_c2_le_1 (c1+c2) c3).
+    split.
+    rewrite <- (real_plus_unit real_0).
+    apply real_le_le_plus_le; auto.
+    split; auto; split; auto.
+  Qed.
 
   Definition point_point_mid (p1 : ^euclidean 2) (p2 : ^euclidean 2) : ^euclidean 2.
     destruct (split_euclidean2 p1) as [x1 [y1 _]].
@@ -285,24 +343,143 @@ Context {types : RealTypes} { casofReal : ComplArchiSemiDecOrderedField_Real typ
     induction n.
     - simpl. 
       intros pt spt.
+
+      (* break down the assumptions *)
       apply Exists_cons_hd.
-      destruct (insideHull pt) as [c1 [c2 [c3 [ptc123 [c1pos [c2pos c3pos]]]]]]. auto.
+      destruct (insideHull pt) as [c1 [c2 [c3 [ptc123 valid123]]]]. auto.
+      pose proof (weights123_le_1 _ _ _ valid123) as [c1le1 [c2le1 c3le1]].
+      destruct valid123 as [c1pos [c2pos [c3pos c123sum]]].
+
+      assert (real_0 <= c1 + c2) as c12pos.
+      rewrite <- (real_plus_unit real_0).
+      apply real_le_le_plus_le; auto.
+
+      assert (real_0 <= c2 + c3) as c23pos.
+      rewrite <- (real_plus_unit real_0).
+      apply real_le_le_plus_le; auto.
+
+      assert (c1 + c2 <= real_1) as c12sum.
+      apply (weights12_c1_le_1 (c1+c2) c3).
+      split; auto; split; auto; right; auto.
+  
+      assert (c2 + c3 <= real_1) as c23sum.
+      rewrite real_plus_assoc in c123sum.
+      apply (weights12_c2_le_1 c1 (c2+c3)).
+      split; auto; split; auto; right; auto.
+  
       destruct (split_euclidean2 pt) as [px [py pxy]].
       (* destruct (split_euclidean2 ST_v1) as [x1 [y1 _]].
       destruct (split_euclidean2 ST_v2) as [x2 [y2 _]].
-      destruct (split_euclidean2 ST_v3) as [x3 [y3 _]]. *)
-  
+      destruct (split_euclidean2 ST_v3) as [x3 [y3 _]]. *)  
       rewrite pxy in ptc123.
       unfold ST_weighted_pt in ptc123.
       simpl in ptc123.
       unfold make_euclidean2 in ptc123.
       injection ptc123.
       intros py_sum px_sum.
-      unfold ball_to_subset.
+
+      (* simplify the goal *)
+      rewrite pxy.
+      unfold make_ball, make_euclidean2.
+      unfold ball_to_subset, euclidean_max_dist, euclidean_minus, euclidean_plus.
+      simpl.
+      rewrite px_sum, py_sum.
+      clear px_sum py_sum pxy spt pt ptc123 px py.
+
+      (* simplify ring subexpressions in the goal *)
+      assert (c1 * - real_1 + c2 * - real_1 + c3 * real_1 + - real_0 = (- c1) + (- c2) + c3) as Temp.
+      ring. rewrite Temp; clear Temp.
+      assert (c1 * real_1 + c2 * - real_1 + c3 * - real_1 + - real_0 = c1 + (- c2) + (- c3)) as Temp.
+      ring. rewrite Temp; clear Temp.
+
+      apply real_max_le_le_le.
+      destruct (real_total_order (- c1 + - c2 + c3) real_0).
+      + rewrite abs_neg_id_neg.
+        ring_simplify.
+        unfold real_minus.
+        rewrite <- real_plus_unit.
+        rewrite (real_plus_comm real_0).
+        apply real_le_le_plus_le.
+        auto.
+        destruct c3pos as [c3pos | c3eq0].
+        left.
+        rewrite <- real_0_neg_eq.
+        apply real_lt_anti; auto.
+        right.
+        rewrite <- c3eq0.
+        apply real_0_neg_eq. 
+        auto.
+      + destruct H.
+        rewrite H.
+        assert (abs real_0 = real_0) as Temp.
+        apply abs_zero; auto.
+        rewrite Temp; clear Temp.
+        left.
+        apply real_1_gt_0.
+        rewrite abs_pos_id.
+        ring_simplify.
+        unfold real_minus.
+        rewrite <- real_plus_unit.
+        apply real_le_le_plus_le.
+        assert (- c1 + - c2 = - (c1 + c2)) as Temp.
+        ring. rewrite Temp; clear Temp.
+        destruct c12pos as [c12pos | c12eq0].
+        left.
+        rewrite <- real_0_neg_eq.
+        apply real_lt_anti; auto.
+        right.
+        rewrite <- c12eq0.
+        apply real_0_neg_eq. 
+        auto.
+        left; auto.
+      + apply real_max_le_le_le.
+        destruct (real_total_order (c1 + - c2 + - c3) real_0).
+        * rewrite abs_neg_id_neg.
+          ring_simplify.
+          rewrite real_plus_assoc.
+          rewrite <- real_plus_unit.
+          apply real_le_le_plus_le.
+          destruct c1pos as [c1pos | c1eq0].
+          left.
+          rewrite <- real_0_neg_eq.
+          apply real_lt_anti; auto.
+          right.
+          rewrite <- c1eq0.
+          apply real_0_neg_eq.
+          auto.
+          auto.
+        * destruct H.
+          rewrite H.
+          assert (abs real_0 = real_0) as Temp.
+          apply abs_zero; auto.
+          rewrite Temp; clear Temp.
+          left.
+          apply real_1_gt_0.
+
+          rewrite abs_pos_id.
+          ring_simplify.
+          unfold real_minus.
+          rewrite real_plus_assoc.
+          rewrite <- real_plus_unit.
+          rewrite (real_plus_comm real_0).
+          apply real_le_le_plus_le.
+          auto.
+          assert (- c2 + - c3 = - (c2 + c3)) as Temp.
+          ring. rewrite Temp; clear Temp.
+
+          destruct c23pos as [c23pos | c23eq0].
+          left.
+          rewrite <- real_0_neg_eq.
+          apply real_lt_anti; auto.
+          right.
+          rewrite <- c23eq0.
+          apply real_0_neg_eq. 
+          auto.
+          left; auto.
+        * left. apply real_1_gt_0.
+
+    - intros pt spt.
       
-      
-      inside_ST_hull
-    
   Admitted.
   
 End SierpinskiTriangle.
